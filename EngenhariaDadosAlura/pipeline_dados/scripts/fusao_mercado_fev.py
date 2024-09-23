2e246b3ff6b8fe9dc99ed19e0ff004fe86a4b016
Patricia Correia de Lima Santos <patriciacorreialimma@gmail.com>
2024-09-23 16:01:33 -0300

    Update fusao_mercado_fev.py

--- EngenhariaDadosAlura/pipeline_dados/scripts/fusao_mercado_fev.py
+++ EngenhariaDadosAlura/pipeline_dados/scripts/fusao_mercado_fev.py
@@ -1,19 +1,12 @@
 import json
 import csv
-
-path_json='data_raw/dados_empresaA.json'
-
+#função p ler aqrv empresa armazenava em json
 def leitura_json(path_json):
     dados_json = []
     with open(path_json, 'r') as file:
         dados_json = json.load(file)
     return dados_json
-    
-
-dados_json = leitura_json(path_json)
-print(dados_json[0])
-
-
+#função p ler aqrv empresa armazenava em csv
 def leitura_csv(path_csv):
 
     dados_csv = []
@@ -23,9 +16,19 @@ def leitura_csv(path_csv):
             dados_csv.append(row)
             
     return dados_csv
-    
-dados_json = leitura_json(path_json)
-print(path_json[0])
-
-path_json = leitura_json(path_csv)
+#função p ler aqrv em função do parametro tipo de arquivo informado
+def leitura_dados(path, tipo_dado):
+    lendo = []
+    if tipo_dado == "csv" :
+        lendo = leitura_csv(path)
+    elif tipo_dado  == "json":
+        lendo = leitura_json(path)
+    return lendo
+#indicando local arquivos
+path_json='data_raw/dados_empresaA.json'
+path_csv = 'data_raw/dados_empresaB.csv'
+#aplicando a leitura para os arquivos.
+dados_json = leitura_dados(path_json,'json')
 print(dados_json[0])
+dados_csv = leitura_dados(path_csv,'csv')
+print(dados_csv[0])
